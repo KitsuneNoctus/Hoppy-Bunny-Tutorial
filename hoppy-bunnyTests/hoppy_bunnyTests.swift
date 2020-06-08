ac25d42dd850aa751e8decd249e1e17b43da0a5c
Henry Calderon <theceintiant@gmail.com>
2020-06-07 20:22:45 -0700

    Initial Commit

---
+++ hoppy-bunnyTests/hoppy_bunnyTests.swift
@@ -0,0 +1,34 @@
+//
+//  hoppy_bunnyTests.swift
+//  hoppy-bunnyTests
+//
+//  Created by Henry Calderon on 6/7/20.
+//  Copyright © 2020 Henry Calderon. All rights reserved.
+//
+
+import XCTest
+@testable import hoppy_bunny
+
+class hoppy_bunnyTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
